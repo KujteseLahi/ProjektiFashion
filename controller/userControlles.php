d5c085b5e592d1ec9c0d18efed7f9fc36995cfb5
BleonaH <56068146+BleonaH@users.noreply.github.com>
2021-02-12 21:28:44 +0100

    Add files via upload

---
+++ controller/userControlles.php
@@ -0,0 +1,33 @@
+<?php
+include_once ($_SERVER['DOCUMENT_ROOT'].'/Projekti/models/userMapper.php');
+include_once ($_SERVER['DOCUMENT_ROOT'].'/Projekti/models/userModel.php');
+class UserController
+{
+    public function InsertUser($username,$email, $passwordU, $roli)
+    {
+        //therrasim funksionet qe bejne kalkulimin e kerkeses
+        //insert user ndatabase
+        $user = new User($username,$email, $passwordU, $roli);
+        $userMapper2 = new UserMapper();
+        $userMapper2->Insert($user);
+        return true;
+
+
+
+    }
+
+    public function GetUser($username,$email, $passwordU , $roli)
+    {
+      $user = new User($username,$email, $passwordU, $roli);
+        $userMapper3= new UserMapper();
+        $userMapper3->LogIn($user);
+        $res = $userMapper3->LogIn($user);
+        if($res){
+            return true;
+        }else{
+            return false;
+        }
+        
+      
+    }
+}
