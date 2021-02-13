a4ddb2bfe529ab6449f4d8f4595bc1408dbc37d2
KujteseLahi <63053562+KujteseLahi@users.noreply.github.com>
2021-02-13 21:01:13 +0100

    Update ProductAdmin.php

--- ProductAdmin.php
+++ ProductAdmin.php
@@ -11,7 +11,7 @@ $produktet = $produkt->getP();
     <title>Wear-It</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/adminPage.css">
+    <link rel="stylesheet" href="productClient.css">
 
 
 </head>
