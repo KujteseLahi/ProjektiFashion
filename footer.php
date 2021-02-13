e234cef55abfe35767886587b34815e5a798ebf4
BleonaH <56068146+BleonaH@users.noreply.github.com>
2021-02-13 03:37:55 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,51 @@
+<footer>
+                      
+    <div class="footer-elements">
+      <div class="footer logo">
+        <img style="width: 150px;height: 150px;" src="images/bagicon.png">
+  
+      </div>
+    <div class="footer explore">
+      <h2 class="footer-heading" >Explore</h2>
+      
+      <ul>
+        
+        <li> <a href="#" >Home</a></li>
+        <li><a href="#" >Products</a></li>
+        <li><a href="aboutUs.php" >About Us</a></li>
+        
+      </ul>
+
+    </div>
+    <div class="footer info">
+      <h2 class="footer-heading">Visit Us</h2>
+        
+      <ul>  
+        <li></i>Autostrada Tiranë - Elbasan, Tiranë</li>
+        <li></i> Rr. Agim Ramadani, Prishtine</li>
+        </ul>
+    </div>
+        <div class="footer contact">
+        <h2 class="footer-heading">Contact Us</h2>
+          <ul>
+        <li> +355 4 418-640</li>
+        <li> +383 44 202-472</li>
+        <li> google@gmail.com</li>
+       </ul>
+    </div>
+      <div class="footer follow">
+        <h2 class="footer-heading">Follow us</h2>
+         
+        <ul>
+      
+          <li><i class="fab fa-facebook"></i>Fashion Fab</li>
+          <li><i class="fab fa-instagram"></i>FashionFab</li>
+                
+        </ul>
+    </div>
+
+    </div>
+   <p style="padding-left: 105px; font-weight: bolder;"> @2020 All Right Reserved.</p> 
+  </div>
+ 
+  </footer>
---
+++ menu.php
@@ -0,0 +1,12 @@
+<div class="navbar">
+           
+            <nav class="stroke">
+              <ul>
+                <li><a href="home.html">Home</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="aboutUs.html">About Us</a></li>
+                <li><a href="cart.html"><i class="fa fa-shopping-bag" aria-hidden="true"></i></a></li>
+                
+              </ul>
+            </nav>
+          </div>
