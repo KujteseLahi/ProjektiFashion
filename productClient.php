2c6cf3259ac5e9893e1ec3a6660a36fe423a6091
KujteseLahi <63053562+KujteseLahi@users.noreply.github.com>
2021-02-14 05:04:05 +0100

    Update productClient.php

--- productClient.php
+++ productClient.php
@@ -62,56 +62,6 @@ $produktet = $produkt->getP();
                         
             
         </div>
-           
-          
-        <footer>
-                      
-          <div class="footer-elements">
-            <div class="footer logo">
-              <img  style="width: 150px;height: 150px;" src="images/bagicon.png">
-        
-            </div>
-          <div class="footer explore">
-            <h2 class="footer-heading" >Explore</h2>
-            
-            <ul>
-            <li> <a href="home.php" >Home</a></li>
-              <li><a href="productClient.php" >Products</a></li>
-              <li><a href="aboutUs.php" >About Us</a></li>
-           </ul>
-      
-          </div>
-          <div class="footer info">
-            <h2 class="footer-heading">Visit Us</h2>
-              
-            <ul>  
-              <li></i>Autostrada Tiranë - Elbasan, Tiranë</li>
-              <li></i> Rr. Agim Ramadani, Prishtine</li>
-              </ul>
-          </div>
-              <div class="footer contact">
-              <h2 class="footer-heading">Contact Us</h2>
-                <ul>
-              <li> +355 4 418-640</li>
-              <li> +383 44 202-472</li>
-              <li> google@gmail.com</li>
-             </ul>
-          </div>
-            <div class="footer follow">
-              <h2 class="footer-heading">Follow us</h2>
-               
-              <ul>
-            
-                <li><i class="fab fa-facebook"></i>Fashion Fab</li>
-                <li><i class="fab fa-instagram"></i>FashionFab</li>
-                      
-              </ul>
-          </div>
-      
-          </div>
-         <p style="padding-left: 105px; font-weight: bolder; color: aliceblue;"> @2020 All Right Reserved.</p> 
-        </div>
-       
-        </footer>
+          <?php include 'footer.php' ?>
     </body>
 </html>
