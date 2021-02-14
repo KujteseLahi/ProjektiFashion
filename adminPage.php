14af3e898547b3cc31a9925effc85a64563f952d
KujteseLahi <63053562+KujteseLahi@users.noreply.github.com>
2021-02-14 06:22:35 +0100

    Update adminPage.php

--- adminPage.php
+++ adminPage.php
@@ -7,7 +7,7 @@ $produktet = $produkt->getKomentet();
 <html lang="en">
 
 <head>
-    <title>Wear-It</title>
+    <title>FashionGlam</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="css/adminPage.css">
